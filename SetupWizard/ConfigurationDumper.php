fdob/ezpublish-kernel

Make Configurator service available independently of DistributionBundle

eZ/Bundle/EzPublishLegacyBundle/SetupWizard/ConfigurationDumper.php
@@ -9,7 +9,7 @@
 
 namespace eZ\Bundle\EzPublishLegacyBundle\SetupWizard;
 
-use Sensio\Bundle\DistributionBundle\Configurator\Configurator;
+use eZ\Bundle\EzPublishLegacyBundle\Configurator\Configurator;
 use eZ\Publish\Core\MVC\Symfony\ConfigDumperInterface;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Filesystem\Filesystem;
@@ -22,9 +22,9 @@ class ConfigurationDumper implements ConfigDumperInterface
     protected $fs;
 
     /**
-     * @var \Sensio\Bundle\DistributionBundle\Configurator\Configurator
+     * @var \eZ\Bundle\EzPublishLegacyBundle\Configurator\Configurator
      */
-    protected $sensioConfigurator;
+    protected $configurator;
 
     /**
      * Path to root dir (kernel.root_dir)
@@ -48,13 +48,13 @@ class ConfigurationDumper implements ConfigDumperInterface
      */
     protected $envs;
 
-    public function __construct( Filesystem $fs, array $envs, $rootDir, $cacheDir, Configurator $sensioConfigurator = null )
+    public function __construct( Filesystem $fs, array $envs, $rootDir, $cacheDir, Configurator $configurator )
     {
         $this->fs = $fs;
         $this->rootDir = $rootDir;
         $this->cacheDir = $cacheDir;
         $this->envs = array_fill_keys( $envs, true );
-        $this->sensioConfigurator = $sensioConfigurator;
+        $this->configurator = $configurator;
     }
 
     /**
@@ -105,16 +105,13 @@ class ConfigurationDumper implements ConfigDumperInterface
         }
 
         // Handling %secret%
-        if ( $this->sensioConfigurator !== null )
-        {
-            $this->sensioConfigurator->mergeParameters(
-                array(
-                    // Step #1 is SecretStep
-                    'secret' => $this->sensioConfigurator->getStep( 1 )->secret
-                )
-            );
-            $this->sensioConfigurator->write();
-        }
+        $this->configurator->mergeParameters(
+            array(
+                // Step #1 is SecretStep
+                'secret' => $this->configurator->getStep( 1 )->secret
+            )
+        );
+        $this->configurator->write();
 
         $this->clearCache();
     }

eZ/Bundle/EzPublishLegacyBundle/Tests/SetupWizard/ConfigurationDumperTest.php
@@ -29,6 +29,11 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
      */
     private $envs;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $configurator;
+
     protected function setUp()
     {
         parent::setUp();
@@ -37,6 +42,13 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
         $this->configDir = __DIR__ . '/config';
         @mkdir( $this->configDir );
         $this->envs = array( 'dev', 'prod' );
+        $this->configurator = $this->getMock(
+            'eZ\\Bundle\\EzPublishLegacyBundle\\Configurator\\Configurator',
+            array(),
+            array(),
+            '',
+            false
+        );
     }
 
     protected function tearDown()
@@ -129,7 +141,23 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
             ->will( $this->returnValue( false ) );
         $this->expectsCacheClear();
 
-        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir );
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'mergeParameters' )
+            ->with( $this->isType( 'array' ) );//@todo Change to check the value as well
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'getStep' )
+            ->with( 1 )
+            ->will( $this->returnValue( (object)array( 'secret' => 'mysecret value' ) ) );
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'write' )
+            ->with();
+
+        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir, $this->configurator );
         $dumper->dump( $configArray );
         $this->assertConfigFileValid( $configArray );
         $this->assertEnvConfigFilesValid();
@@ -152,7 +180,23 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
         $this->expectBackup();
         $this->expectsCacheClear();
 
-        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir );
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'mergeParameters' )
+            ->with( $this->isType( 'array' ) );//@todo Change to check the value as well
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'getStep' )
+            ->with( 1 )
+            ->will( $this->returnValue( (object)array( 'secret' => 'mysecret value' ) ) );
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'write' )
+            ->with();
+
+        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir, $this->configurator );
         $dumper->dump( $configArray, ConfigDumperInterface::OPT_BACKUP_CONFIG );
         $this->assertConfigFileValid( $configArray );
         $this->assertEnvConfigFilesValid();

ezsystems/LegacyBridge

SetupWizard/ConfigurationDumper.php
@@ -48,7 +48,7 @@ class ConfigurationDumper implements ConfigDumperInterface
      */
     protected $envs;
 
-    public function __construct( Filesystem $fs, array $envs, $rootDir, $cacheDir, Configurator $sensioConfigurator = null )
+    public function __construct( Filesystem $fs, array $envs, $rootDir, $cacheDir, Configurator $sensioConfigurator )
     {
         $this->fs = $fs;
         $this->rootDir = $rootDir;
@@ -105,16 +105,13 @@ class ConfigurationDumper implements ConfigDumperInterface
         }
 
         // Handling %secret%
-        if ( $this->sensioConfigurator !== null )
-        {
-            $this->sensioConfigurator->mergeParameters(
-                array(
-                    // Step #1 is SecretStep
-                    'secret' => $this->sensioConfigurator->getStep( 1 )->secret
-                )
-            );
-            $this->sensioConfigurator->write();
-        }
+        $this->sensioConfigurator->mergeParameters(
+            array(
+                // Step #1 is SecretStep
+                'secret' => $this->sensioConfigurator->getStep( 1 )->secret
+            )
+        );
+        $this->sensioConfigurator->write();
 
         $this->clearCache();
     }

Tests/SetupWizard/ConfigurationDumperTest.php
@@ -29,6 +29,11 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
      */
     private $envs;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $configurator;
+
     protected function setUp()
     {
         parent::setUp();
@@ -37,6 +42,13 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
         $this->configDir = __DIR__ . '/config';
         @mkdir( $this->configDir );
         $this->envs = array( 'dev', 'prod' );
+        $this->configurator = $this->getMock(
+            'Sensio\\Bundle\\DistributionBundle\\Configurator\\Configurator',
+            array(),
+            array(),
+            '',
+            false
+        );
     }
 
     protected function tearDown()
@@ -129,7 +141,23 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
             ->will( $this->returnValue( false ) );
         $this->expectsCacheClear();
 
-        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir );
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'mergeParameters' )
+            ->with( $this->isType( 'array' ) );//@todo Change to check the value as well
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'getStep' )
+            ->with( 1 )
+            ->will( $this->returnValue( (object)array( 'secret' => 'mysecret value' ) ) );
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'write' )
+            ->with();
+
+        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir, $this->configurator );
         $dumper->dump( $configArray );
         $this->assertConfigFileValid( $configArray );
         $this->assertEnvConfigFilesValid();
@@ -152,7 +180,23 @@ class ConfigurationDumperTest extends \PHPUnit_Framework_TestCase
         $this->expectBackup();
         $this->expectsCacheClear();
 
-        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir );
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'mergeParameters' )
+            ->with( $this->isType( 'array' ) );//@todo Change to check the value as well
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'getStep' )
+            ->with( 1 )
+            ->will( $this->returnValue( (object)array( 'secret' => 'mysecret value' ) ) );
+
+        $this->configurator
+            ->expects( $this->once() )
+            ->method( 'write' )
+            ->with();
+
+        $dumper = new ConfigurationDumper( $this->fs, $this->envs, __DIR__, $this->cacheDir, $this->configurator );
         $dumper->dump( $configArray, ConfigDumperInterface::OPT_BACKUP_CONFIG );
         $this->assertConfigFileValid( $configArray );
         $this->assertEnvConfigFilesValid();