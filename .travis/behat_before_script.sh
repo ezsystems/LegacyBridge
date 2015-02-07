efb349f21f9b16c091a553c6220cc7eec8212ad2
Bertrand Dunogier <bd@ez.no>
2015-02-07 17:10:57 +0100

    BDD attempt 2

--- .travis/behat_before_script.sh
+++ .travis/behat_before_script.sh
@@ -1,6 +1,40 @@
 #!/bin/sh
-echo "> Require legacy-bridge"
-composer require legacy-bridge:dev-master
+echo "> Configuring legacy-bridge"
+
+composer require --no-update "legacy-bridge:dev-master"
+
+php ./bin/.travis/enablelegacybundle.php
+
+# Load LegacyBundle routes
+echo << EOF >> ezpublish/config/routing.yml
+
+_ezpublishLegacyRoutes:
+    resource: '@EzPublishLegacyBundle/Resources/config/routing.yml'
+
+EOF
+
+# legacy mode
+echo << EOF >> ezpublish/config/ezpublish.yml
+
+ez_publish_legacy:
+    system:
+        behat_site_admin:
+            legacy_mode: true
+
+EOF
+
+# setup firewall rule
+echo << EOF >> ezpublish/config/security.yml
+
+        ezpublish_setup:
+            pattern: ^/ezsetup
+            security: false
+
+EOF
+
+# Enabled eztpl templating engine
+sed -i "s/'twig'/'eztpl', 'twig'/" ezpublish/config/config.yml
+
 
 ./bin/.travis/prepare_ezpublish.sh
 
