03e092befa6b974b76bf23c642a5fa60e70f17cd
Bertrand Dunogier <bd@ez.no>
2015-05-13 17:13:06 +0200

    EZP-24278: integrated Legacy REST API in Kernel

---
+++ bundle/Rest/ResponseWriter.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Bundle\EzPublishLegacyBundle\Rest;
+
+use ezcMvcRequest;
+use ezcMvcResponse;
+use ezcMvcResultCache;
+use ezcMvcResultContent;
+use ezcMvcResultStatusObject;
+use ezpKernelRest;
+use ezpKernelResult;
+use ezpRestHttpResponseWriter;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseWriter extends ezpRestHttpResponseWriter
+{
+    public function handleResponse()
+    {
+        // process all headers
+        $this->processStandardHeaders();
+        if ( $this->response->cache instanceof ezcMvcResultCache )
+        {
+            $this->processCacheHeaders();
+        }
+        if ( $this->response->content instanceof ezcMvcResultContent )
+        {
+            $this->processContentHeaders();
+        }
+
+        // process the status headers through objects
+        if ( !$this->response->status instanceof ezcMvcResultStatusObject )
+        {
+            $responseCode = 200;
+        }
+        else
+        {
+            $responseCode = $this->response->status->code;
+        }
+
+        // automatically add content-length header
+        $this->headers['Content-Length'] = strlen( $this->response->body );
+
+        ezpKernelRest::setResponse(
+            new ezpKernelResult( $this->response->body, array( 'headers' => $this->headers, 'statusCode' => $responseCode ) )
+        );
+    }
+}
