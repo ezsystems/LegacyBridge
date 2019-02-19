a94d9b80056b7e97b34790eb2b17b70477ffc5cb
Gunnstein Lye <gunnstein.lye@ez.no>
2019-02-19 20:35:02 +0100

    Update bundle/Command/LegacyInitCommand.php
    Co-Authored-By: andrerom <andre.romcke@gmail.com>

--- bundle/Command/LegacyInitCommand.php
+++ bundle/Command/LegacyInitCommand.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the LegacySrcSymlinkCommand class.
+ * File containing the LegacyInitCommand class.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
